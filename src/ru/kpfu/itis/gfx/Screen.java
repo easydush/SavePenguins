1b40ff1dc0ff7723cd0338a477df00344d50ff33
Isis <izidacasillas@gmail.ru>
2019-12-21 11:08:48 +0300

    Initial commit

---
+++ src/ru/kpfu/itis/gfx/Screen.java
@@ -0,0 +1,10 @@
+package ru.kpfu.itis.gfx;
+
+public class Screen {
+    public static final int MAP_WIDTH =64;
+    public static final int MAP_WIDTH_MASK = MAP_WIDTH-1;
+
+    public int[] tiles = new int[MAP_WIDTH*MAP_WIDTH];
+    public int[] colours = new int[MAP_WIDTH*MAP_WIDTH*4];
+
+}
