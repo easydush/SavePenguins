8e6fea4e7b5afcc924cbfe27681a88b76253f6c4
easydush <izidacasillas@gmail.ru>
2019-12-21 18:59:42 +0300

    changed Game.java

--- src/ru/kpfu/itis/game/Game.java
+++ src/ru/kpfu/itis/game/Game.java
@@ -1,7 +1,13 @@
 package ru.kpfu.itis.game;
 
+import ru.kpfu.itis.game.entities.Player;
+import ru.kpfu.itis.game.entities.PlayerMP;
 import ru.kpfu.itis.game.gfx.Screen;
 import ru.kpfu.itis.game.gfx.SpriteSheet;
+import ru.kpfu.itis.game.level.Level;
+import ru.kpfu.itis.game.network.GameClient;
+import ru.kpfu.itis.game.network.GameServer;
+import ru.kpfu.itis.game.network.messages.Message00Login;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,55 +16,116 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
 public class Game extends Canvas implements Runnable {
-    public static final int WIDTH = 320 ;
-    public static final int HEIGHT = WIDTH/12*9;
-    public static final int SCALE = 3;
-    public static final String NAME = "Save penguins";
 
-    private JFrame frame;
+    private static final long serialVersionUID = 1L;
+
+    public static final int WIDTH = 160;
+    public static final int HEIGHT = WIDTH / 12 * 9;
+    public static final int SCALE = 3;
+    public static final String NAME = "Game";
+    public static final Dimension DIMENSIONS = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
+    public static Game game;
+
+    public JFrame frame;
+
+    private Thread thread;
 
     public boolean running = false;
     public int tickCount = 0;
 
-    private BufferedImage image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
-    private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+    private int[] colours = new int[6 * 6 * 6];
+
     private Screen screen;
-    private SpriteSheet spriteSheet = new SpriteSheet("res/sprite_sheet.png");
-    public Game() {
-        setMinimumSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
-        setMaximumSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
-        setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
+    public InputHandler input;
+    public WindowHandler windowHandler;
+    public Level level;
+    public Player player;
 
-        frame = new JFrame(NAME);
+    public GameClient socketClient;
+    public GameServer socketServer;
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
+    public boolean debug = true;
+    public boolean isApplet = false;
 
-        frame.add(this,BorderLayout.CENTER);
-        frame.pack();
+    public void init() {
+        game = this;
+        int index = 0;
+        for (int r = 0; r < 6; r++) {
+            for (int g = 0; g < 6; g++) {
+                for (int b = 0; b < 6; b++) {
+                    int rr = (r * 255 / 5);
+                    int gg = (g * 255 / 5);
+                    int bb = (b * 255 / 5);
 
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+                    colours[index++] = rr << 16 | gg << 8 | bb;
+                }
+            }
+        }
+        screen = new Screen(WIDTH, HEIGHT, new SpriteSheet("/sprite_sheet.png"));
+        input = new InputHandler(this);
+        level = new Level("/levels/water_test_level.png");
+        player = new PlayerMP(level, 100, 100, input, JOptionPane.showInputDialog(this, "Please enter a username"),
+                null, -1);
+        level.addEntity(player);
+        if (!isApplet) {
+            Message00Login loginMessage = new Message00Login(player.getUsername(), player.x, player.y);
+            if (socketServer != null) {
+                socketServer.addConnection((PlayerMP) player, loginMessage);
+            }
+            loginMessage.writeData(socketClient);
+        }
+    }
+
+    public synchronized void start() {
+        running = true;
+
+        thread = new Thread(this, NAME + "_main");
+        thread.start();
+        if (!isApplet) {
+            if (JOptionPane.showConfirmDialog(this, "Do you want to run the server") == 0) {
+                socketServer = new GameServer(this);
+                socketServer.start();
+            }
+
+            socketClient = new GameClient(this, "localhost");
+            socketClient.start();
+        }
+    }
+
+    public synchronized void stop() {
+        running = false;
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void run() {
         long lastTime = System.nanoTime();
-        double nsPerTick = 1000000000d/60d;
-        int frames = 0;
+        double nsPerTick = 1000000000D / 60D;
+
         int ticks = 0;
+        int frames = 0;
 
         long lastTimer = System.currentTimeMillis();
         double delta = 0;
-        while (running){
+
+        init();
+
+        while (running) {
             long now = System.nanoTime();
-            delta += (now-lastTime)/nsPerTick;
+            delta += (now - lastTime) / nsPerTick;
             lastTime = now;
             boolean shouldRender = true;
-            while(delta >= 1){
+
+            while (delta >= 1) {
                 ticks++;
                 tick();
-                delta--;
+                delta -= 1;
                 shouldRender = true;
             }
 
@@ -68,48 +135,79 @@ public class Game extends Canvas implements Runnable {
                 e.printStackTrace();
             }
 
-            if(shouldRender) {
+            if (shouldRender) {
                 frames++;
                 render();
             }
-            if(System.currentTimeMillis() - lastTimer >= 1000){
-                lastTimer += 100;
+
+            if (System.currentTimeMillis() - lastTimer >= 1000) {
+                lastTimer += 1000;
+                debug(DebugLevel.INFO, ticks + " ticks, " + frames + " frames");
                 frames = 0;
                 ticks = 0;
             }
         }
     }
 
-    public synchronized void start() {
-        running = true;
-        new Thread(this).start();
-    }
-
-    public synchronized void stop() {
-        running = false;
-    }
-
-    public void tick(){
+    public void tick() {
         tickCount++;
-        for(int i = 0;i < pixels.length; i++){
-          pixels[i] = i +  tickCount;
-    }}
+        level.tick();
+    }
 
-    public void render(){
+    public void render() {
         BufferStrategy bs = getBufferStrategy();
-        if(bs == null){
+        if (bs == null) {
             createBufferStrategy(3);
+            return;
+        }
+
+        int xOffset = player.x - (screen.width / 2);
+        int yOffset = player.y - (screen.height / 2);
+
+        level.renderTiles(screen, xOffset, yOffset);
+        level.renderEntities(screen);
+
+        for (int y = 0; y < screen.height; y++) {
+            for (int x = 0; x < screen.width; x++) {
+                int colourCode = screen.pixels[x + y * screen.width];
+                if (colourCode < 255)
+                    pixels[x + y * WIDTH] = colours[colourCode];
+            }
         }
 
         Graphics g = bs.getDrawGraphics();
-
-        g.drawImage(image,0,0,getWidth(),getHeight(),null);
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
         g.dispose();
         bs.show();
     }
 
-    public static void main(String[] args){
-        new Game().start();
+    public static long fact(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * fact(n - 1);
+        }
     }
 
+    public void debug(DebugLevel level, String msg) {
+        switch (level) {
+            default:
+            case INFO:
+                if (debug) {
+                    System.out.println("[" + NAME + "] " + msg);
+                }
+                break;
+            case WARNING:
+                System.out.println("[" + NAME + "] [WARNING] " + msg);
+                break;
+            case SEVERE:
+                System.out.println("[" + NAME + "] [SEVERE]" + msg);
+                this.stop();
+                break;
+        }
+    }
+
+    public static enum DebugLevel {
+        INFO, WARNING, SEVERE;
+    }
 }
